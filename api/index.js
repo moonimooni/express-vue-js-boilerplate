39f6ddf4ccbc1e436b4fadae0807667f4d38dfdf
moonimooni <heewonmoon@day1company.co.kr>
2022-01-31 00:25:56 +0900

    fix: use proxy router

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const { router } = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use('/', router);
+app.use('/api', router);
 app.listen(config.api.port, () => {
   console.log(`server on ${config.api.port}`);
 });
